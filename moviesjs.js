2b520bff6056924b89f40cdd1598f9dbb703a6f9
Gerald Montero <geraldmontero12@gmail.com>
2022-02-10 14:56:51 -0600

    after lunch work

--- moviesjs.js
+++ moviesjs.js
@@ -1,172 +1,232 @@
-"use strict";
-(() => {
-// Target URL for Glitch
-    const URL = "https://ancient-juicy-balloon.glitch.me/movies";
-// OMDB URL
-    const OMDB_URL = "http://www.omdbapi.com/";
+{
+    // const $ = require('jquery');
+    const THEMOVIEDB_API_TOKEN = "67b9c6b38ecdf29bee715b3e3eef0d84"
 
-// FETCH REQUEST
-// Get the array of movie objects and send to the render function
-    const getmovies = () => {
-        return fetch(URL)
-            .then(resp => resp.json())
-            .then(data => {
-                rendermovies(data);
-                console.log(data);
-            })
-            .catch(err => console.error(err));
-    }
-// Initial Fetch Request that retreives the data and renders it is Delayed in order to show off our loading animation
-// This setTimeout is instructor approved without tears
-    setTimeout(getmovies, 4000);
-
-// RENDER FUNCTION
-// Take each movie (object) and turn each attribute into HTML Elements for display
-    const rendermovies = (movies) => {
-        // Empty the HTML String on each call
-        let moviesHTML = "";
-        // Iterate through each movie
-        for (let movie of movies) {
-            // Create a new "Movie" Div, with nested divs for each listed attribute
-            moviesHTML += '<div class="movie">' +
-                '<div class="title">' + movie.title + '</div>' +
-                '<img class="poster" src="' + movie.poster + '" alt="a movie poster">' +
-                '<div class="year"> Released: ' + movie.year + '</div>' +
-                '<div class="director"> Directed by ' + movie.director + '</div>' +
-                '<div class="rating"> Rating: ' + movie.rating + '</div>' +
-                '<div class="genre"> Genre: ' + movie.genre + '</div>' +
-                '<div class="actors"> Starring: ' + movie.actors + '</div>' +
-                '<div class="plot">' + movie.plot + '</div>' +
-                // EDIT & DELETE BUTTONS
-                // Rendered with the particular movie ID, this allows targeting of the class "edit-btn" or "delete-btn"
-                // for simplified event function, while still allowing *this* particular movie to be targeted to PATCH or DELETE
-                '<button class="edit-btn"  data-id="' + movie.id + '">Edit</button>' +
-                '<button class="delete-btn" data-id="' + movie.id + '">Delete</button>' +
-                // Closing individual "movie" div
-                '</div>'
+    const harkenDatabase = "https://enshrined-icy-harpymimus.glitch.me/movies"
+    $(document).ready(() => {
+        //--- Content for Main tab
+        function getPoster(title) {
+            return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${THEMOVIEDB_API_TOKEN}&query=${title}`)
+                .then(response => response.json())
+                .then(movie => {
+                    console.log(movie)
+                    let posterPath = movie.results[0].poster_path
+                    return "http://image.tmdb.org/t/p/w500" + posterPath
+                })
+                .catch(error => console.error(error))
         }
-        // Set the HTML of the target to the given string of elements & data.
-        $('#movies').html(moviesHTML);
-        // EDIT CLICK FUNCTION
-        $('.edit-btn').click(function () {
-            populateEdit($(this).data("id"));
+
+        function harkenMovies() {
+            fetch(harkenDatabase)
+                .then(response => response.json())
+                .then(movies => {
+                    let promises = [];
+                    for (let movie of movies) {
+                        let {title, rating, id} = movie;
+                        $('#main').append(`
+                            <div id="${title}" class="card my-2" style="width: 18rem;">
+                                <img class="card-img-top" data- src="" alt="movie poster">
+                                <div id="${id}" class="card-body">
+                                    <h3>${title}</h3> 
+                                    <p>  ${rating} <i class="fas fa-star"></i></p>
+                                    <div class="d-flex justify-content-around">
+                                        <button data-movieid="${id}" data-movietitle="${title}" data-rating="${rating}" class="edit btn btn-dark">Edit</button>
+                                        <button data-movieid="${id}" class="deletion btn btn-dark">Delete</button>
+                                    </div>
+                                </div>
+                            </div>
+                        `);
+                        promises.push(getPoster(title))
+                    }
+                    Promise.all(promises)
+                        .then(function (movieUrls) {
+                            let images = $('img');
+                            for (let i = 0; i < movieUrls.length; i++) {
+                                images[i].src = movieUrls[i];
+                            }
+                        })
+                        .then(gatherEditDeleteButtons())
+                })
+
+                .catch(error => console.error(error))
+        }
+
+        //Initial call to populate Main tab
+        harkenMovies();
+
+        //---- Add a Movie Tab
+        let getSearchPoster = function () {
+            let film = $('#term').val();
+            // Check for blank entry
+            if (film === '') {
+                $('#poster').html('<div class="alert"><strong>Oops!</strong> Try adding something into the search field.</div>');
+            } else {
+                $('#poster').html('<div class="alert"><strong>Loading...</strong></div>');
+                $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + film + "&callback=?",
+                    function (json) {
+                        //Found Title
+                        if (json !== "Nothing found.") {
+                            //console.log(json);
+                            //Show user result
+                            $('#poster').html('<div class="card-body"><p class="text-white">You have added: <strong>' + json.results[0].title +
+                                '</strong></p></div><div class="card" style="width: 18rem;"><img class="card-img-top" src=\"http://image.tmdb.org/t/p/w500/'
+                                + json.results[0].poster_path + '\" class=\"img-responsive\" alt="movie poster"></div>');
+                            //Show alternate response when title not found
+                        } else {
+                            $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=goonies&callback=?",
+                                function (json) {
+                                    //console.log(json);
+                                    $('#poster').html('<div class="alert"><p>We\'re afraid nothing was found for that search.</p></div>' +
+                                        '<p>Perhaps you were looking for The Goonies?</p><img alt="movie poster" id="thePoster" src=\"http://image.tmdb.org/t/p/'
+                                        + json.results[0].poster_path + '\" class="img-responsive">');
+                                });
+                        }
+
+                        let newMovie = {
+                            title: json.results[0].title,
+                            rating: document.getElementById('rate').value
+                        }
+
+                        function addMovie(newMovie) {
+                            return fetch(harkenDatabase, {
+                                method: "POST",
+                                headers: {"Content-Type": "application/json"},
+                                body: JSON.stringify(newMovie)
+                            })
+                                .then(response => response.json())
+                                .then(data => {
+                                    console.log("Success!")
+                                    return data;
+                                })
+                                .catch(console.error);
+                        }
+
+                        addMovie(newMovie).then(() => {
+                            return fetch(harkenDatabase)
+                                .then(response => response.json())
+                                .then(console.log)
+                                .then ($('#main').empty())
+                                .then (harkenMovies())
+                                .catch(console.error);
+                        })
+                    });
+            }
+            return false;
+        }
+
+        $('#search').click(function(){
+            getSearchPoster()
         });
-        // DELETE CLICK FUNCTION
-        $('.delete-btn').click(function () {
-            deleteMovie($(this).data("id"));
-        });
-    }
-// SUB-NAVIGATION: FORM CONTROL
-// Each Tab toggles its given form, as well as the tab's icon for a left or right arrow.
-    $('#search-tab').click(() => {
-        $('#search-form').toggleClass('hide');
-        $('#search-right').toggleClass('hide');
-        $('#search-left').toggleClass('hide');
-    });
-    $('#add-tab').click(() => {
-        $('#add-form').toggleClass('hide');
-        $('#add-right').toggleClass('hide');
-        $('#add-left').toggleClass('hide');
-    });
-    $('#edit-tab').click(() => {
-        $('#edit-form').toggleClass('hide');
-        $('#edit-right').toggleClass('hide');
-        $('#edit-left').toggleClass('hide');
-    });
-// FORM SUBMISSION: ADD FUNCTION
-// On click of "Add" Button, will create a new movie object from the return of the OMDB API
-// and send the new object to the createMovie() function, which posts it to our db
-    $('#add-movie').click(function (e) {
-        e.preventDefault();
-        // Takes the form values of the title and rating
-        let title = $('#add-title').val();
-        let rating = $('#add-rating').val();
-        console.log(title)
-        // GET Request from the OMDB API searching by our input title
-        fetch(`http://www.omdbapi.com/?i=${title}&apikey=`)
-            .then(resp => resp.json())
-            .then(data => {
-                // Parsing through the returned attributes to match them to our Movie database attributes
-                console.log(data);
+        $('#term').keyup(function (event) {
+            if (event.keyCode === 13) {
+                getSearchPoster();
+            }
+        });// --- End of Add a Movie tab
 
-                let title = data.Title;
-                let year = data.Year;
-                let director = data.Director;
-                let poster = data.Poster;
-                let plot = data.Plot;
-                let genre = data.Genre;
-                let actors = data.Actors;
-                // Creating a new movie object with the desired attributes
-                let newMovie = {title, poster, rating, year, director, genre, actors, plot};
-                // calling the POST function with the new movie
-                createMovie(newMovie);
+        //----Edit Movie
+        function editCard(movieIdNumber, newTitle, newRating){
+            let editedTitle = newTitle.replace(/\w\S*/g, function(txt)
+            {
+                if (txt.match(/^(e|y|de|lo|los|la|las|do|dos|da|das|del|van|von|bin|le)$/gi)) return txt.toLowerCase();
+                else return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+            $(`#${movieIdNumber} h3`).replaceWith(`<h3>${editedTitle}</h3>`);
+            $(`#${movieIdNumber} p`).replaceWith(`<p>   ${newRating} <i class="fas fa-star"></i></p>`);
+        }
+
+        function editMovie(movieIdNumber, newTitle, newRating){
+            editCard(movieIdNumber, newTitle, newRating);
+            let myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            let raw = JSON.stringify({"title": newTitle,"rating": newRating,"id": movieIdNumber});
+            const requestOptions = {
+                method: 'PUT',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+            let targetUrl = harkenDatabase + '/' + movieIdNumber;
+            return fetch(targetUrl, requestOptions)
+        }
+
+        //----Delete Movie
+        function deleteMovie(movieIdNumber){
+            const requestOptions ={
+                method: 'DELETE',
+                redirect: 'follow'
+            };
+            let targetUrl = harkenDatabase + '/' + movieIdNumber;
+            console.log(targetUrl)
+            return fetch(targetUrl, requestOptions)
+        }
+
+        //---- Gather all of the Edit and Delete buttons
+        function gatherEditDeleteButtons(){
+            const editButtons = document.querySelectorAll(".edit");
+            const deleteButtons = document.querySelectorAll(".deletion")
+
+            // Add listeners to the selected buttons
+            editButtons.forEach((editButton, key) =>{
+                editButton.addEventListener("click", (e) => {
+                    let selectedMovie = e.target.dataset['movieid'];
+                    // console.log(selectedMovie);
+                    let modal = document.getElementById("myModal");
+                    $("#new-movie-title").attr("placeholder", e.target.dataset['movietitle']);
+                    $("#new-rating").attr("placeholder", e.target.dataset['rating']);
+                    modal.style.display = "block";
+
+                    let newTitle = '';
+                    let newRating;
+                    //---- Get newTitle and newRating from Modal window
+                    $('#submit').click(function (){
+                        newTitle = $('#new-movie-title').val()
+                        newRating = $('#new-rating').val()
+                        console.log(newTitle, newRating, selectedMovie)
+                        modal.style.display = "none"
+                        return editMovie(selectedMovie, newTitle, newRating)
+                            .then(response => response.json())
+                            .then(console.log)
+                            //.then ($('#main').empty())
+                            //.then (setTimeout(harkenMovies(),2000))
+                            .catch(console.error);
+                    })
+                })
+            });
+
+            //---- Close modal window if user clicks "cancel"
+            $('#cancel').on("click", (e) => {
+                let modal = document.getElementById("myModal");
+                modal.style.display = "none";
             })
-            .catch(err => console.error(err));
+
+            deleteButtons.forEach((deleteButton, key) =>{
+                deleteButton.addEventListener("click", (e)=>{
+                    let selectedMovie = e.target.dataset['movieid'];
+                    deleteMovie(selectedMovie)
+                        .then(response => response.json())
+                        .then(console.log)
+                        .then ($('#main').empty())
+                        .then (harkenMovies())
+                        .catch(console.error);
+                })
+            });
+
+        }
+
+        //---- Loading Animation
+        $(window).on('load', function () {
+            //Animate loader off screen
+            $(".se-pre-con").fadeOut("slow");
+        });
+
     });
-// FORM POPULATE: EDIT FUNCTION
-// Takes the movie ID from an edit-button and populates the edit form with the current data for that movie
-    const populateEdit = (movieID) => {
-        // First, get the information for that movie ID
-        fetch(`${URL}/${movieID}`)
-            .then(resp => resp.json())
-            // Take the movie attributes and set the values within the edit form to match the value of each attribute
-            .then(movie => {
-                $('#edit-title').val(movie.title);
-                $('#edit-release').val(movie.year);
-                $('#edit-director').val(movie.director);
-                $('#edit-rating').val(movie.rating);
-                $('#edit-actors').val(movie.actors);
-                $('#edit-genre').val(movie.genre);
-                $('#edit-plot').val(movie.plot);
-                $('#movie-id').val(movieID);
-            })
-            .catch(err => console.error(err));
-    }
-// FORM SUBMISSION: EDIT FUNCTION
-// On click of "Apply" Button, will create an object with the inputs on the edit form and send to the editMovie() function
-    $('#edit-apply').click(function (e) {
-        e.preventDefault();
-        let title = $('#edit-title').val();
-        let rating = $('#edit-rating').val();
-        let year = $('#edit-release').val();
-        let director = $('#edit-director').val();
-        let actors = $('#edit-actors').val();
-        let genre = $('#edit-genre').val();
-        let plot = $('#edit-plot').val();
-        let id = $('#movie-id').val();
-        let editedMovie = {id, title, rating, year, genre, director, actors, plot};
-        console.log(editedMovie);
-        editMovie(editedMovie);
-    });
-// POST REQUEST
-// Takes in the new movie and uses POST to add it to the JSON file
-    const createMovie = (movie) => {
-        let options = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(movie)
-        }
-        // after posting, getMovies is called again to update the list
-        fetch(URL, options).then(resp => resp.json()).then(getmovies).catch(err => console.error(err));
-    }
-// PATCH REQUEST
-// Takes the movie from the Edit Form and uses PATCH to change the values of it's attributes in the JSON file
-    const editMovie = (movie) => {
-        let options = {
-            method: 'PATCH',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(movie)
-        }
-        return fetch(`${URL}/${movie.id}`, options).then(resp => resp.json()).then(getmovies).catch(err => console.error(err));
-    }
-// DELETE REQUEST
-// Takes the movie ID from a delete-button and uses DELETE to to remove it from the JSON file
-    const deleteMovie = (movieID) => {
-        let options = {
-            method: 'DELETE',
-            headers: {'Content-Type': 'application/json'},
-        }
-        return fetch(`${URL}/${movieID}`, options).then(getmovies).catch(err => console.error(err));
-    }
-})();
+}
+
+
+
+
+
+
+
+
 
